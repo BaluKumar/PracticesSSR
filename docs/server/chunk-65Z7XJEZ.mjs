9e8ae6da6562d30cbf683b409a877302c85d0e56
Balu R <balu@centizen.com>
2024-11-04 18:34:45 +0530

    doc changes for deploy

---
+++ docs/server/chunk-65Z7XJEZ.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as e,b as r,c as i,d as n,e as p,l as m,n as a,o as s,r as f,x as c}from"./chunk-RMPKOU42.mjs";var d=(()=>{class o{constructor(){this.title="PracticesSSR"}static{this.\u0275fac=function(t){return new(t||o)}}static{this.\u0275cmp=e({type:o,selectors:[["app-root"]],standalone:!0,features:[p],decls:2,vars:0,template:function(t,A){t&1&&(r(0,"div"),n(1,` balaji love naveen
+`),i())}})}}return o})();var l=[];var u={providers:[c(l),s()]};var C={providers:[f()]},v=m(u,C);var x=()=>a(d,v),T=x;export{T as a};
